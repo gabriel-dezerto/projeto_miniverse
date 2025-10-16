8cdb0e85428565b7c08839bbd7c9f2cdfba2cdc1
gabriel-dezerto <dezertogabriel@gmail.com>
2025-10-16 13:34:59 -0300

    criando JS do site

---
+++ miniverse/js/index.js
@@ -0,0 +1,35 @@
+//Script para dar destaque à marca clicada no header na página de produtos
+document.addEventListener("DOMContentLoaded", () => {
+    const hash = window.location.hash;
+
+    if (hash) {
+        const section = document.querySelector(hash);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" }); //Define como será a rolagem até a seção correta
+            section.classList.add("destaque");
+
+            setTimeout(() => {
+                section.classList.remove("destaque");
+            }, 3000);
+        }
+    }
+});
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
